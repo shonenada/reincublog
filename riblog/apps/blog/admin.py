91ed2296679f832f12ffaf161b6586482cd13659
James Cooke <james@fublo.net>
2012-10-06 14:36:06 +0100

    Small batch of admin powerups.

---
+++ riblog/apps/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date')
+    list_filter = ['published_date']
+    search_fields = ['title']
+    date_heirarchy = 'published_date'
+admin.site.register(Post, PostAdmin)
--- riblog/apps/blog/models.py
+++ riblog/apps/blog/models.py
@@ -1,15 +1,29 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from tinymce import models as tinymce_models
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
 
 
 class Post(models.Model):
     author = models.ForeignKey(User)
     title = models.CharField(max_length=200)
     featured_image = models.ImageField(upload_to='featured', blank=True)
-    content = models.TextField()
+    content = tinymce_models.HTMLField()
     published_date = models.DateTimeField(auto_now_add=True, editable=True)
-    categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-published_date',)
